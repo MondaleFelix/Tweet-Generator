ab09e53fb18c835c798f5146cd0feb8ec66cf143
MondaleFelix <mondalefelix@yahoo.com>
2019-11-03 20:47:03 -0800

    returns word based on frequency

--- sample.py
+++ sample.py
@@ -2,13 +2,32 @@ import histogram as hist
 import sys
 import random
 
-text = "Mondale is the coolest person alive"
+text = "one fish two fish red fish blue fish"
 
 def return_word(histogram):
-	random_index = random.randint(0, len(histogram) - 1)
-	return list(histogram)[random_index]
+	random_index = random.random()
+	for key, value in histogram.items():
+		if random_index < value:
+			return key
+
+
 
 test_histogram = hist.histogram(text)
 
 
 print(return_word(test_histogram))
+
+def generate_probability(histogram):
+	total_words = sum(histogram.values())
+	counter = 0
+	new_histogram = {}
+	for key, value in histogram.items():
+		probability = value / total_words
+		counter += probability
+		new_histogram[key] =  counter
+	return new_histogram
+
+
+
+print(generate_probability(test_histogram))
+print(return_word(generate_probability(test_histogram)))
