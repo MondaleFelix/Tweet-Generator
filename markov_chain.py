cd80f9ef1b35e05d2f8e092780179e21e3b72899
MondaleFelix <mondalefelix@yahoo.com>
2019-11-05 09:27:06 -0800

    returned a sentence based on probability

--- markov_chain.py
+++ markov_chain.py
@@ -1,14 +1,12 @@
+import random
+
+
 text = "one fish two fish red fish blue fish"
 
 def get_following_words(text):
 	words_list = text.split()
 	following_words_dict = {}
-
-
 	counter = 1
-
-
-
 
 	for word in words_list:
 		if word is words_list[len(words_list) - 1]:
@@ -19,11 +17,27 @@ def get_following_words(text):
 			following_words_dict[word].append(words_list[counter])
 		else:
 			following_words_dict[word].append(words_list[counter])
-		# print(following_words_dict)
 		counter += 1
 
+	return following_words_dict
 
-	print(following_words_dict)
+def return_sentence(hist, num_of_words, starting_word):
+	
+	sentence = [starting_word]
+
+	next_word = get_next_word(hist[starting_word])
+
+	while len(sentence) is not num_of_words:
+		sentence.append(next_word)
+		print(next_word)
+		next_word = get_next_word(hist[next_word])
+		print(next_word)
+
+	print(sentence)
 
 
-get_following_words(text)
+
+def get_next_word(array):
+	return array[random.randint(0,len(array) - 1)]
+
+return_sentence(get_following_words(text), 10 , "fish")
